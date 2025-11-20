b3f5544da0b97e32e741f6962fa73340e8812565
0krk01234 <ruizfeez99@gmail.com>
2025-11-20 16:37:59 +0700

    Lab1.1

---
+++ LAB1/Lab1point1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; //import 
+
+      public class Lab1point1 { 
+         public static void main(String[] args) { 
+
+         // สร้าง Scanner 
+
+         Scanner input = new Scanner(System.in);
+
+         int Number1 = input.nextInt(); //รับค่า num1
+         int Number2 = input.nextInt(); //รับค่า num2
+
+         System.out.println(Number1 + Number2); //นำค่าทั้ง2มาบวกกัน
+         // ปิด scanf 
+         input.close();
+         }
+      }
