a74a696d77ffa3068dd8186b085113c8997e74d8
Natthawut Juntaya <ruizfeez99@gmail.com>
2025-11-20 15:50:02 +0700

    Add files via upload

---
+++ LAB1.java
@@ -0,0 +1,20 @@
+   // import 
+   import java.util.Scanner; 
+      public class LAB1 { // BIG BOX
+         public static void main(String[] args) { 
+
+         // สร้าง Scanner 
+
+         Scanner input = new Scanner(System.in);
+
+         System.out.print("Enter Number1: "); 
+         int Number1 = input.nextInt(); 
+
+         System.out.print("Enter Number2: "); 
+         int Number2 = input.nextInt(); 
+         
+         System.out.println("\n "+ (Number1 + Number2)); 
+         // ปิด scanf 
+         input.close();
+ } 
+}
