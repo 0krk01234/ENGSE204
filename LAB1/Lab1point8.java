936493499cdf259876e3d3600aae0714cfe8e9f4
0krk01234 <ruizfeez99@gmail.com>
2025-11-21 12:17:15 +0700

    21.11.68 LAB

--- LAB1/Lab1point8.java
+++ LAB1/Lab1point8.java
@@ -1,28 +1,28 @@
-    import java.util.Scanner;
+    import java.util.Scanner; // import
 
     public class Lab1point8 {
         public static void main(String[] args) {
 
-            Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in); // สร้าง Scanner
 
-            int N = input.nextInt();
+            int N = input.nextInt(); //  รับค่าตัวแปร N
 
-            int[] numBer = new int[N];
+            int[] numBer = new int[N]; // กำหนดตัวแปร numBer ให้มีจำนวน arry เท่ากับ ตัวแปร N ที่รับค่ามา
 
-            for (int i = 0 ; i < N ; i ++) {
+            for (int i = 0 ; i < N ; i ++) { // loop รับค่าเข้าไปใน arry
 
-                numBer[i] = input.nextInt();
+                numBer[i] = input.nextInt(); //รับค่า ใน arry numBer
 
-            }
+            } // end for
 
-            int Max = 0 ;
+            int Max = 0 ; // สร้างตัวแปร Max = 0
 
-            for (int i = 0 ; i < N ; i++) {
-                if (numBer[i] > Max) {
-                    Max = numBer[i] ;
-                }
-            }
-            System.out.println(Max);
+            for (int i = 0 ; i < N ; i++) { // loop หาค่า Max ใน arry
+                if (numBer[i] > Max) { // จะเข้าเงื่อนไขเมื่อ numBer[i] มีค่ามากกว่า Max
+                    Max = numBer[i] ; // กำหนดให้ Max มีค่าเท่ากัย arry numBer
+                } // end if
+            } // end for
+            System.out.println(Max); // แสดงค่า Max
             input.close(); // ปิด Scanner
-        }  
-}
+        }  // end main
+} // end class
