06028ced8c44d5676f53e71e4e5ee72c67720e98
0krk01234 <ruizfeez99@gmail.com>
2025-11-22 22:56:15 +0700

    update lab

--- LAB1/Lab1point13.java
+++ LAB1/Lab1point13.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
 public class Lab1point13 {
-    
+    public static void main(String[] arry2miti) {
+        Scanner input = new Scanner(System.in);
+
+        int r = input.nextInt();
+        int c = input.nextInt();
+
+        int[][] arr = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = input.nextInt();
+            }
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (arr[i][j] == 1) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+        input.close();
+    }
 }
