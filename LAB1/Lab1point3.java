290e1b774f1f5c6dc9cd3a58a3e744ceb8d4cabc
0krk01234 <ruizfeez99@gmail.com>
2025-11-23 21:00:52 +0700

    fix 1.3 last

--- LAB1/Lab1point3.java
+++ LAB1/Lab1point3.java
@@ -6,9 +6,9 @@
 
         Scanner input = new Scanner(System.in); 
 
-        int numBer = input.nextInt(); 
+        int numbee = input.nextInt(); 
 
-        if (numBer % 2 == 0) {
+        if (numbee % 2 == 0) {
             System.out.println("Even"); 
         }
 
