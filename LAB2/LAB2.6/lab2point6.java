852619adc0842111ce82443cd5be9bf2b52533e8
0krk01234 <ruizfeez99@gmail.com>
2025-12-02 15:12:59 +0700

    finish LAB2

--- LAB2/LAB2.6/lab2point6.java
+++ LAB2/LAB2.6/lab2point6.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
+
 public class lab2point6 {
 
-    public static void main(String[] bankkkbalanceee) {
+    public static void main(String[] bankkkkbalance) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter name");
         String name = input.nextLine();
 
         System.out.println("Balance1");
-        double balance1 = input.nextLine();
+        double balance1 = input.nextDouble();
 
         System.out.println("Balance2");
-        double balance2 = input.nextLine();
+        double balance2 = input.nextDouble();
 
-        BankAccount B1 = BankAccount(name , balance2);
+        BankAccount B1 = new BankAccount(name , balance1);
+
+        B1.deposit(balance2);
+        B1.displaySummary();
     }
-    
 }
 
 class BankAccount {
@@ -23,18 +26,16 @@ class BankAccount {
     double balance;
 
     BankAccount(String name , double balance2){
-        this.ownerName = name ;
-        this.balance = balance2 ;
+        this.ownerName = name;
+        this.balance = balance2;
     }
 
-    public void deposit(double balance2){
-        double amount = balance2 + this.balance ;
+    public void deposit(double amount){
+        this.balance += amount;
     }
 
     public void displaySummary(){
-
-        double amount = deposit();
         System.out.println(this.ownerName);
-        System.out.printf("Balance = %.2f" , amount);
+        System.out.printf("Balance = %.2f\n", this.balance);
     }
 }
